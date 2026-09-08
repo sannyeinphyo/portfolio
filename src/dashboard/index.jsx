41983352a3b312264881dd0111e5f35fc77dfb6c
San Nyein Phyo <snp@mit.com.mm>
2026-09-08 16:03:34 +0630

    fix performace

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,10 +1,13 @@
-import About from "./about";
+const About = lazy(() => import('./about'));
+const Home = lazy(() => import('./home'));
+const ContactSection = lazy(() => import('./contact'));
+const SideQuest = lazy(() => import('./side_quest'));
+// import About from "./about";
 // import Home from "./home";
 // import DownloadSection from "./download";
 // import Project from "./project";
-const Home = lazy(() => import('./home'))
-import ContactSection from "./contact";
-import SideQuest from "./side_quest";
+// import ContactSection from "./contact";
+// import SideQuest from "./side_quest";
 import { Navigate } from "react-router-dom";
 
 export const dashboardRoute = [
